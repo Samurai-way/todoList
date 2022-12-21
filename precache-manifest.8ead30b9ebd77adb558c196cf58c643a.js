d23fc91bb76db16ab6b188dc0dddff5a6bfdf8b8
Samurai-way <olegmoishevych@gmail.com>
2022-12-21 22:30:17 +0200

    Updates

---
+++ precache-manifest.8ead30b9ebd77adb558c196cf58c643a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c037168f9ffc32afd8f6d6a97705b39",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "ecf83bb068faf662ffcc",
+    "url": "/todoList/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "959f2d1e2e2d79fdfd82",
+    "url": "/todoList/static/js/2.5b413278.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todoList/static/js/2.5b413278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecf83bb068faf662ffcc",
+    "url": "/todoList/static/js/main.a33108f3.chunk.js"
+  },
+  {
+    "revision": "f16ac80ea54676d2b112",
+    "url": "/todoList/static/js/runtime-main.a87cb681.js"
+  }
+]);
